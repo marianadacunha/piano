1786e5b1792f8ae97bf24c426cd34044e9f953c5
Mariana Marques <marianadacunhamarques@gmail.com>
2020-11-18 11:57:19 -0300

    create more consts

--- js/main.js
+++ js/main.js
@@ -1,5 +1,8 @@
 const whiteKey = [a, s, d, f, g, h, j];
 const blackKey = [w, e, t, y, u];
+const selectKeys = document.querySelectorAll('div.key');
+const selectWhiteKeys = document.querySelectorAll('div.white');
+const selectBlackKeys = document.querySelectorAll('div.black');
 
 function playNote(key) {
     const noteAudio = document.getElementById(key.dataset.note);
