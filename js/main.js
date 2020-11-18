6214ffd29f6dd6f9a7d8a3f9b5750d3f428c3859
Mariana Marques <marianadacunhamarques@gmail.com>
2020-11-18 11:50:32 -0300

    add arrays to store the content of the keys

--- js/main.js
+++ js/main.js
@@ -1,1 +1,7 @@
- 
+const whiteKey = [a, s, d, f, g, h, j];
+const blackKey = [w, e, t, y, u];
+
+function playNote(key) {
+    const noteAudio = document.getElementById(key.dataset.note);
+    noteAudio.play();
+  }
